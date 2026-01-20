6a238f340d8e59e15e15041c87bc785e70eaea9a
LiamAnthonyTiongV <tiong.lv@obmontessori.edu.ph>
2026-01-20 13:54:31 +0800

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,35 @@
+function add() {
+    var num1 = document.getElementById
+    ('num1').value;
+    var num2 = document.getElementById
+    ('num2').value;
+    var sum = num1 + Number
+        window.alert(sum);
+}
+
+function diff() {
+    var num1 = document.getElementById
+    ('num1').value;
+    var num2 = document.getElementById
+    ('num2').value;
+    var diff = num1 - num2
+        window.alert(diff);
+}
+
+function multiply() {
+    var num1 = document.getElementById
+    ('num1').value;
+    var num2 = document.getElementById
+    ('num2').value;
+    var multiply = Number(num1) * Number(num2) 
+        window.alert(multiply);
+}
+
+function divide() {
+    var num1 = document.getElementById
+    ('num1').value;
+    var num2 = document.getElementById
+    ('num2').value;
+    var divide = Number(num1) / Number(num2) 
+        window.alert(divide);
+}
